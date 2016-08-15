ffeb670752e920e9d95db31cc7ddeca2409e4e16
Arvin John Tomacruz <actomacruz@voyagerinnovation.com>
2016-08-16 02:37:32 +0800

    group foldering in xcode bundle

---
+++ MQTTSampleApp/MQTTSampleApp/MQTTManager.swift
@@ -0,0 +1,9 @@
+//
+//  MQTTManager.swift
+//  MQTTSampleApp
+//
+//  Created by Arvin John Tomacruz on 16/08/2016.
+//  Copyright © 2016 Voyager Innovations Inc. All rights reserved.
+//
+
+import Foundation
