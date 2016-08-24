82ce90166fafd31275c2510fb99eb1e05b6878b9
Arvin John Tomacruz <actomacruz@voyagerinnovation.com>
2016-08-24 18:37:43 +0800

    reset files

--- MQTTCodenames/MQTTConstants.swift
+++
@@ -1,9 +0,0 @@
-//
-//  MQTTConstants.swift
-//  MQTTCodenames
-//
-//  Created by Arvin John Tomacruz on 16/08/2016.
-//  Copyright © 2016 Voyager Innovations Inc. All rights reserved.
-//
-
-import Foundation
