bf293b62c26347d4fbc427b9b31cf19d6789f4a7
Arvin John Tomacruz <actomacruz@voyagerinnovation.com>
2016-09-02 01:21:51 +0800

    fix UI

--- MQTTCodenames/MQTTGameRoomViewController.swift
+++ MQTTCodenames/MQTTGameRoomViewController.swift
@@ -10,5 +10,28 @@ import UIKit
 
 class MQTTGameRoomViewController: UIViewController {
 
+    @IBOutlet weak var scrollView: UIScrollView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: #selector(keyboardWillShow(_:)), name: UIKeyboardWillShowNotification, object: nil)
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: #selector(keyboardWillHide(_:)), name: UIKeyboardWillHideNotification, object: nil)
+    }
+    
+    deinit {
+        NSNotificationCenter.defaultCenter().removeObserver(self, name: UIKeyboardWillShowNotification, object: nil)
+        NSNotificationCenter.defaultCenter().removeObserver(self, name: UIKeyboardWillHideNotification, object: nil)
+    }
+    
+    func keyboardWillShow(aNotification: NSNotification) {
+        let keyboardSize = aNotification.userInfo![UIKeyboardFrameEndUserInfoKey]?.CGRectValue().size
+        let contentInsets = UIEdgeInsetsMake(self.scrollView.contentInset.top, 0.0, (keyboardSize?.height)! + 10.0, 0.0)
+        self.scrollView.contentInset = contentInsets;
+    }
+    
+    func keyboardWillHide(aNotification: NSNotification) {
+        let contentInsets = UIEdgeInsetsMake(self.scrollView.contentInset.top, 0.0, 0.0, 0.0)
+        self.scrollView.contentInset = contentInsets;
+    }
 
 }
